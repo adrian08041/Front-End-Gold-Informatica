67466156c189c61de531ec3f77ffe379b3d1a867
Adrian <adrianmessias62@gmail.com>
2025-03-23 20:02:45 -0300

    feat:added login with google and visual color changes

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,55 +1,101 @@
-import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, Percent, PercentIcon, ShoppingCartIcon } from "lucide-react";
+"use client";
+
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
 import { SheetContent, Sheet, SheetTrigger, SheetHeader } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
 
 const Header = () => {
+  const { status } = useSession();
+
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+
   return (
-    <Card className="flex items-center justify-between p-5 p-[1.875rem]">
+    <Card className="border-preto2 flex items-center justify-between bg-black p-5 p-[1.875rem]">
       <Sheet>
         <SheetTrigger asChild>
-        <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+          <Button size="icon">
+            <MenuIcon />
+          </Button>
         </SheetTrigger>
-        
-        
-        <SheetContent side={"left"}>
-            <SheetHeader className="text-lg font-semibold">Menu</SheetHeader>
-            
-            <div className="mt-2 flex gap-2 flex-col">
-                
-                <Button variant={"outline"} className="w-full justify-start gap-2">
-                    <LogInIcon />
-                    Fazer Login
-                </Button>
-                <Button variant={"outline"} className="w-full justify-start gap-2">
-                    <HomeIcon size={16}/>
-                    Inicio
-                </Button>
-                <Button variant={"outline"} className="w-full justify-start gap-2">
-                    <PercentIcon size={16}/>
-                    Ofertas
-                </Button>
-                <Button variant={"outline"} className="w-full justify-start gap-2">
-                    <ListOrderedIcon size={16}/>
-                    Catàlago
-                </Button>
-                
-            </div>
 
-            
-        
-        
+        <SheetContent className="bg-backgroundItems" side={"left"}>
+          <SheetHeader className="text-lg font-semibold text-white">
+            Menu
+          </SheetHeader>
+
+          <div className="mt-2 flex flex-col gap-2">
+            {status === "unauthenticated" && (
+              <Button
+                onClick={handleLoginClick}
+                variant={"outline"}
+                className="w-full justify-start  border-backgroundItems bg-backgroundItems text-white"
+              >
+                <LogInIcon />
+                Fazer Login
+              </Button>
+            )}
+
+            {status === "authenticated" && (
+              <Button
+                onClick={handleLogoutClick}
+                variant={"outline"}
+                className="w-full justify-start  border-backgroundItems bg-backgroundItems text-white"
+              >
+                <LogOutIcon />
+                Fazer Logout
+              </Button>
+            )}
+
+            <Button
+              variant={"outline"}
+              className="w-full justify-start  border-backgroundItems bg-backgroundItems text-white"
+            >
+              <HomeIcon size={16} />
+              Inicio
+            </Button>
+            <Button
+              variant={"outline"}
+              className="w-full justify-start  border-backgroundItems bg-backgroundItems text-white"
+            >
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+            <Button variant={"outline"}
+              className="bg-backgroundItems border-backgroundItems  text-white w-full  justify-start ">
+              <ListOrderedIcon size={16} />
+              Catàlago
+            </Button>
+          </div>
         </SheetContent>
       </Sheet>
+            <Image
+            src={logo}
+            alt="Gold Informàtica"
+            width={150}
+            height={150}
+            />
+      
 
-      
-      
-      
-      <h1 className="text-2xl font-bold">Gold Informàtica</h1>
-
-      <Button size="icon" variant="outline">
+      <Button size="icon">
         <ShoppingCartIcon />
       </Button>
     </Card>
