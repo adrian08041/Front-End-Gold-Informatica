482bee5241136e914b85e1f5e1e328e9383bdac5
Adrian <adrianmessias62@gmail.com>
2025-03-24 22:10:17 -0300

    chore:  add domain to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "utfs.io/f/baccd841-5118-4a43-b7d9-557effb39e03-otetkp.png",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
