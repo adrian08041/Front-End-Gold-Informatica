3deff83979f68cc35a8560afed1453f268c160cf
Adrian <adrianmessias62@gmail.com>
2025-03-27 16:46:04 -0300

    feat: add second banner home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,25 +1,19 @@
-
-
 import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/product-list";
 
 export default async function Home() {
-
   const deals = await prismaClient.product.findMany({
     where: {
       discountPercentage: {
         gt: 0,
+      },
     },
-  },
   });
 
-
-
-
   return (
-    <div >
+    <div>
       <Image
         src="/assets/banner-ofertas-01.png"
         className="h-auto w-full px-5"
@@ -30,12 +24,19 @@ export default async function Home() {
       />
       <div className="mt-8 px-5">
         <Categories />
-
-        
       </div>
       <div className="mt-8">
-          <ProductList products={deals}/>
-        </div>
+        <p className="pl-5 font-bold uppercase text-white">Ofertas</p>
+        <ProductList products={deals} />
+      </div>
+      <Image
+        src="/assets/banner-mouses.png"
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        height={0}
+        width={0}
+        alt="até 50% de desconto em mouses"
+      />
     </div>
   );
 }
