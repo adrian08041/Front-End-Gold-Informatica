1ad8003e891ac44e779f91bc43bf5d1167819c2f
Adrian <adrianmessias62@gmail.com>
2025-05-24 14:15:53 -0300

    fix: bug auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,10 +7,11 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session, token, user }) {
       session.user = { ...session.user, id: user.id } as {
