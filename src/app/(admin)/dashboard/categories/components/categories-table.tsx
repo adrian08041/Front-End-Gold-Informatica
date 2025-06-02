30baa5fbac3e43f0f9974cfcecddf4c33cc2f7f3
Adrian <adrianmessias62@gmail.com>
2025-06-02 16:49:47 -0300

    feat: add categories table

---
+++ src/app/(admin)/dashboard/categories/components/categories-table.tsx
@@ -0,0 +1,50 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { Prisma } from "@prisma/client";
+
+interface CategoriesTableProps {
+  categories: Prisma.CategoryGetPayload<{
+    include: {
+      products: {
+        select: {
+          id: true;
+        };
+      };
+    };
+  }>[];
+}
+
+const CategoriesTable = ({ categories }: CategoriesTableProps) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="text-white">Nome</TableHead>
+          <TableHead className="text-white">Categoria</TableHead>
+          <TableHead className="text-white">Porcentagem das Vendas</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {categories.map((categories) => (
+          <TableRow key={categories.id}>
+            <TableCell className="text-white">{categories.name}</TableCell>
+
+            <TableCell className="text-white">
+              {categories.products.length}
+            </TableCell>
+            <TableCell className="text-white">0%</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default CategoriesTable;
