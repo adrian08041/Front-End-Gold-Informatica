c72f38dd03a12467855cd21ba49e24e81b008eaa
Adrian <adrianmessias62@gmail.com>
2025-06-29 01:10:57 -0300

    feat: logic with React hooks moved to child component

---
+++ src/app/(admin)/dashboard/products/components/products-page-client.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { PackageIcon } from "lucide-react";
+import { computeProductTotalPrice } from "@/helpers/product";
+import ProductsTable, {
+  ProductWithTotalPriceAndCategory,
+} from "./products-table";
+import AddProductButton from "./add-product-button";
+import { ProductQuery } from "@/service/hooks";
+
+export default function ProductsPageClient() {
+  const { data: response, isLoading } = ProductQuery.useProductQueryKey({
+    page: 0,
+    perPage: 0,
+    name: "",
+  });
+
+  const productsWithTotalPrice: ProductWithTotalPriceAndCategory[] =
+    response?.data.map((product) => ({
+      ...product,
+      totalPrice: computeProductTotalPrice(product),
+    })) || [];
+
+  return (
+    <div className="flex w-full flex-col gap-10 p-10">
+      <Badge className="w-fit gap-1 border-2 border-dourado px-3 py-[0.375rem] text-base uppercase">
+        <PackageIcon size={18} />
+        Produtos
+      </Badge>
+
+      <div className="flex w-full items-center justify-between">
+        <p className="text-lg font-bold text-white">
+          Produtos encontrados: {productsWithTotalPrice.length}
+        </p>
+        <div>
+          <AddProductButton />
+        </div>
+      </div>
+
+      <ProductsTable products={productsWithTotalPrice} />
+    </div>
+  );
+}
